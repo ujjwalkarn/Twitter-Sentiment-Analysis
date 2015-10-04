0127f7d9c635aa60cff20151ba7fac725b6d3c79
Ujjwal Karn <ujwlkarn@gmail.com>
2015-10-04 23:36:45 +0530

    Update extract_twitter_data.py

--- extract_twitter_data.py
+++ extract_twitter_data.py
@@ -43,3 +43,8 @@ twitter_stream = Stream(auth, MyListener())
 
 #change the keyword here
 twitter_stream.filter(track=['#cricket'])
+
+#references:
+#http://marcobonzanini.com/2015/03/02/mining-twitter-data-with-python-part-1/
+#http://adilmoujahid.com/posts/2014/07/twitter-analytics/
+#https://github.com/tweepy/tweepy
